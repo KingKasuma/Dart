255f3f1273c564ba7833149541d9f470573aca81
Mike <lanselot777hailander@gmail.com>
2020-04-28 12:21:51 -0400

    Some fixes

--- 04_tipos_no_comunes/04_future2.dart
+++ 04_tipos_no_comunes/04_future2.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 main(){
 
-  File file = new File(Directory.current.path+"\\assets\\personas.txt");
+  File file = new File(Directory.current.path+"/assets\/personas.txt");
   //print(Directory.current.path);
 
   Future<String> f = file.readAsString(); //Retorna un Future<String>
