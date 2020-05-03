e9933a889a0bd5fbed1f2f172e954abbe8f72dd2
Mike <lanselot777hailander@gmail.com>
2020-05-03 16:38:09 -0400

    Exercise to consume api of col

--- ejercicios/paquetes/bin/main.dart
+++ ejercicios/paquetes/bin/main.dart
@@ -4,7 +4,8 @@ import 'package:paquetes/paquetes.dart' as paquetes;
 
 main(List<String> arguments) {
   
-  paquetes.getReqResp_service();
+  //paquetes.getReqResp_service();
+  paquetes.getPais();
 
 }
 
--- ejercicios/paquetes/lib/paquetes.dart
+++ ejercicios/paquetes/lib/paquetes.dart
@@ -1,6 +1,8 @@
 import 'package:http/http.dart' as http;
 import 'package:paquetes/classes/request_response.dart';
 
+import 'classes/pais.dart';
+
 void getReqResp_service(){
   final url = 'https://reqres.in/api/users?page=2';
   http.get(url).then((response){
@@ -17,4 +19,24 @@ void getReqResp_service(){
     print('id del tercer elemento: ${ resReqRes.data[2].id }');
 
   });
+}
+
+void getPais(){
+  final url = 'https://restcountries.eu/rest/v2/alpha/col';
+  http.get(url).then((response){
+    
+    final col = paisFromJson( response.body );
+    print('==================================');
+    print('Pais ${ col.name } ');
+    print('Pais ${ col.population } ');
+    print('Fronteras');
+    col.borders.forEach((f) => print('     $f'));
+    print('Idioma: ${ col.languages[0].nativeName }');
+    print('Latitude: ${ col.latlng[0] }');
+    print('Longitude: ${ col.latlng[1] }');
+    print('Moneda: ${ col.currencies[0].name }');
+    print('Bandera: ${ col.flag }');
+    print('==================================');
+
+  }).catchError((error) => print(error));
 }
